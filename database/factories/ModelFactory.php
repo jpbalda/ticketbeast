c8cf23ca5a96e48b1eb241adc71dd722028c06c0
Juan Pablo Balda <jpbalda@hotmail.com>
2018-03-31 13:07:44 -0500

    1.4 - Unit Testing Presentation Logic

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Carbon\Carbon;
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
@@ -20,5 +22,20 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Concert::class, function (Faker\Generator $faker) {
+    return [
+        'title' => 'Example Band',
+        'subtitle' => 'with The Fake Openers',
+        'date' => Carbon::parse('+2 weeks'),
+        'ticket_price' => 2000,
+        'venue' => 'The Example Theater',
+        'venue_address' => '123 Example Lane',
+        'city' => 'Fakeville',
+        'state' => 'ON',
+        'zip' => '90210',
+        'additional_information' => 'Some sample additional information.'
     ];
 });
---
+++ tests/unit/ConcertTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Concert;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ConcertTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function can_get_formatted_date()
+    {
+        $concert = factory(Concert::class)->create([
+            'date' => Carbon::parse('2016-12-01 8:00pm'),
+        ]);
+
+        $date = $concert->formatted_date;
+
+        $this->assertEquals('December 1, 2016', $date);
+    }
+}
