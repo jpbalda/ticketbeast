776c779f5cdaafa9d8cbf2a8af6753229b68993f
Juan Pablo Balda <jpbalda@hotmail.com>
2018-03-31 22:00:21 -0500

    2.9 - Handling Failed Charges

--- tests/features/PuchaseTicketsTest.php
+++ tests/features/PuchaseTicketsTest.php
@@ -51,6 +51,24 @@ class PurchaseTicketsTest extends TestCase
     }
 
     /** @test */
+    public function an_order_is_not_create_if_payment_fails()
+    {
+        $this->disableExceptionHandling();
+
+        $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
+
+        $this->orderTickets($concert, [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => 'invalid-payment-token',
+        ]);
+
+        $this->assertResponseStatus(422);
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNull($order);
+    }
+
+    /** @test */
     public function email_is_required_to_purchase_tickets()
     {
         $concert = factory(Concert::class)->create();
