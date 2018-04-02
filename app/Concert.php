4e869c8af3dcddc2ca6bdcf1ca62984ca78792ea
Juan Pablo Balda <jpbalda@hotmail.com>
2018-04-01 21:59:13 -0500

    3.3 - Refusing Orders When There Are No More Tickets

--- app/Concert.php
+++ app/Concert.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\NotEnoughTicketsException;
 
 class Concert extends Model
 {
@@ -42,8 +43,13 @@ class Concert extends Model
 
     public function orderTickets($email, $ticketQuantity)
     {
+        $tickets = $this->tickets()->available()->take($ticketQuantity)->get();
+
+        if ($tickets->count() < $ticketQuantity) {
+            throw new NotEnoughTicketsException();
+        }
+
         $order = $this->orders()->create(['email' => $email]);
-        $tickets = $this->tickets()->take($ticketQuantity)->get();
 
         foreach ($tickets as $ticket) {
             $order->tickets()->save($ticket);
@@ -61,6 +67,6 @@ class Concert extends Model
 
     public function ticketsRemaining()
     {
-        return $this->tickets()->whereNull('order_id')->count();
+        return $this->tickets()->available()->count();
     }
 }
